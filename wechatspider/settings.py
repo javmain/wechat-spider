39c9e4c8e921fed463f5c97d343330d1f81d77f4
server <zhangxuan@rongyitech.com>
2017-09-01 17:11:45 +0800

    fixed

--- wechatspider/settings.py
+++ wechatspider/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['suibian.srleyang.com']
 
 
 # Application definition
@@ -106,7 +106,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR,'wechatspider/static')
+print STATIC_ROOT
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
